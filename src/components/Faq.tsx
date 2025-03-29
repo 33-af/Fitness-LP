12407a6a76894159491f62ab9501e8ff1a356199
Mykhail <gorgefraggggg@gmail.com>
2025-03-29 13:35:03 +0300

    Add footer mobile

--- src/components/Faq.tsx
+++ src/components/Faq.tsx
@@ -7,7 +7,7 @@ const Faq = () => {
   return (
 
 
-    <section className="mx-auto  z-0 relative xl:w-[1244px] xl:mb-20 lg:middle-container lg:mb-[60px]">
+    <section className="mx-auto  z-0 relative xl:w-[1244px] xl:mb-20 lg:middle-container lg:mb-[60px]  max-w-[430] mb-9">
 
       <div className=" left-0 top-[75px]  z-10  bg-[#77060B] w-[505px] h-[292px] blur-[120px] rounded-full absolute  opacity-40 " />
 
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -11,10 +11,10 @@ import YouTubeIcon from '../assets/YouTubeIcon.png';
 const Footer = () => {
     return (
         <footer className=" bg-[#181818]  w-full ">
-            <div className="custom-container xl:pt-[60px]  xl:pb-[67px] lg:pt-8  lg:middle-container">
-                <div className="flex items-start  justify-between gap-2">
-                    <div className="flex flex-col xl:w-[515px] mr-3 lg:w-[361px]">
-                        <img src={FooterLogo} alt="footer-logo" width={151} height={63} className='xl: w-[151px] h-[63px] lg:w-[135px] h-[22px] mb-4' />
+            <div className="custom-container xl:pt-[60px]  xl:pb-[67px] lg:pt-8  lg:middle-container max-w-[430] py-8  px-6">
+                <div className="flex flex-col items-center justify-between gap-4 max-w-[430px] w-full sm:flex-row items-start">
+                    <div className="flex flex-col xl:w-[515px] r-3 m lg:w-[361px] r-3  max-w-[430px]  ">
+                        <img src={FooterLogo} alt="footer-logo" width={151} height={63} className='xl: w-[151px] h-[63px] lg:w-[135px] h-[22px] mb-4 max-w-[430px] max-w-[132px] max-h-[53px]' />
                         <div className=" text-[#888888] text-justify xl:font-medium mb-6 text-base lg:mb-4 text-[12px]">
                             Transform Your Body with FitMaker, Your Trusted Partner in Fitness. With Over <span className='text-[#D90A14] '>5 Years</span>  of Experience, We Offer Expert Coaching, Tailored Workout Plans, and Comprehensive Nutritional Guidance. <span className='text-[#D90A14] '>Join Our Community</span> and Start Your Journey Towards a Healthier, Stronger You. Ready to Make a Change?
                         </div>
@@ -26,60 +26,79 @@ const Footer = () => {
                         </div>
                     </div>
 
-                    <div className="flex  gap-1  xl:w-[515px] h-[301px] lg:w-[361px] h-[260px] ">
+                    <div className="xl:w-[515px] flex  gap-1  h-[301px]  lg:w-[361px] flex  gap-1   h-[260px]  max-w-[382px] w-full max-h-[280px]  justify-center items-start">
 
-                        <div className="flex flex-col xl:w-[169px] lg:w-[118px] ">
-                            <h2 className='text-[#D90A14] text-[20px] text-center mb-9 lg:mb-6'>Company</h2>
-                            <div className="text-[#888888] text-[14px] text-center ">
-                                <p className=' xl:mb-6 lg:mb-[20px]'>About us</p>
-                                <p className=' xl:mb-6 lg:mb-[20px]'>Our Services</p>
-                                <p className=' xl:mb-6 lg:mb-[20px]'>Careers</p>
-                                <p className=' xl:mb-6 lg:mb-[20px]'>Blog</p>
-                                <p className=' xl:mb-6 lg:mb-[20px]'>Testimonial</p>
-                                <p >Contact Us</p>
+                        <div className="flex flex-col xl:w-[169px] lg:w-[118px] max-w-[124px] w-full  ">
+                            <h2 className="text-[#D90A14] text-[20px] text-center xl:mb-9 lg:mb-6  max-[430px]:mb-6">
+                                Company
+                            </h2>
+                            <div className="text-[#888888] text-[14px] text-center">
+                                {["About us", "Our Services", "Careers", "Blog", "Testimonial", "Contact Us"].map((text, index) => (
+                                    <p key={index} className="xl:mb-6 lg:mb-6 max-[430px]:mb-[20px] last:mb-0">
+                                        {text}
+                                    </p>
+                                ))}
                             </div>
                         </div>
 
-                        <div className="flex flex-col xl:w-[169px] lg:w-[118px] ">
-                            <h2 className='text-[#D90A14] text-[20px] text-center mb-9 lg:mb-6'>Resources</h2>
-                            <div className="text-[#888888] text-[14px] text-center ">
-                                <p className=' xl:mb-6 lg:mb-[20px]'>Fitness tools</p>
-                                <p className=' xl:mb-6 lg:mb-[20px]'>Workout Videos</p>
-                                <p className=' xl:mb-6 lg:mb-[20px]'>Nutrition Guides</p>
-                                <p className=' xl:mb-6 lg:mb-[20px]'>FAQ</p>
-                                <p className=' xl:mb-6 lg:mb-[20px]'>Success Stories</p>
-                                <p>Membership </p>
+                        <div className="flex flex-col xl:w-[169px] lg:w-[118px] max-w-[124px] w-full  ">
+                            <h2 className="text-[#D90A14] text-[20px] text-center xl:mb-9 lg:mb-6  max-[430px]:mb-6">
+                                Resources
+                            </h2>
+                            <div className="text-[#888888] text-[14px] text-center">
+                                {[
+                                    "Fitness tools",
+                                    "Workout Videos",
+                                    "Nutrition Guides",
+                                    "FAQ",
+                                    "Success Stories",
+                                    "Membership",
+                                ].map((text, index) => (
+                                    <p key={index} className="xl:mb-6 lg:mb-6 max-[430px]:mb-[20px] last:mb-0">
+                                        {text}
+                                    </p>
+                                ))}
                             </div>
                         </div>
 
 
-                        <div className="flex flex-col xl:w-[169px] lg:w-[118px] ">
-                            <h2 className='text-[#D90A14] text-[20px] text-center mb-9 lg:mb-6'>Programs</h2>
-                            <div className="text-[#888888] text-[14px] text-center ">
-                                <p className=' xl:mb-6 lg:mb-[20px]'>Weight Loss</p>
-                                <p className=' xl:mb-6 lg:mb-[20px]'>Building Muscles</p>
-                                <p className=' xl:mb-6 lg:mb-[20px]'>Home Workout</p>
-                                <p className=' xl:mb-6 lg:mb-[20px]'>Gym Plan</p>
-                                <p className=' xl:mb-6 lg:mb-[20px]'>Our Plans</p>
-                                <p>Fitness group Us</p>
+
+                        <div className="flex flex-col xl:w-[169px] lg:w-[118px] max-w-[124px] w-full  ">
+                            <h2 className="text-[#D90A14] text-[20px] text-center xl:mb-9 lg:mb-6  max-[430px]:mb-6">
+                                Programs
+                            </h2>
+                            <div className="text-[#888888] text-[14px] text-center">
+                                {[
+                                    "Weight Loss",
+                                    "Building Muscles",
+                                    "Home Workout",
+                                    "Gym Plan",
+                                    "Our Plans",
+                                    "Fitness group Us",
+                                ].map((text, index) => (
+                                    <p key={index} className="xl:mb-6 lg:mb-6 max-[430px]:mb-[20px] last:mb-0">
+                                        {text}
+                                    </p>
+                                ))}
                             </div>
                         </div>
+
 
                     </div>
 
-                    <div className="flex flex-col w-[190px] h-[187px]  lg:h-[158px]">
+                    <div className="flex flex-col xl:w-[169px] lg:w-[118px] max-w-[124px] w-full  ">
                         <h2 className='text-[#D90A14] text-[20px] text-center mb-9 lg:mb-6'>Contact Us</h2>
                         <div className="flex items-start flex-col">
-                            <div className="flex items-center gap-1 mb-6">
+                            <div className="flex items-center gap-1 mb-6 w-[190px]">
                                 <img src={Location} alt="Location-icon" width={24} height={24} />
                                 <p className='text-[#888888] text-[12px] ml-1'>Usa - Washngton DC</p>
                             </div>
-                            <div className="flex items-center gap-1 mb-6">
+                            <div className="flex items-center gap-1 mb-6 w-[190px]">
                                 <img src={PhoneIcon} alt="Phone-icon" width={24} height={24} />
                                 <p className='text-[#888888] text-[12px] ml-1'>1234-56789C</p>
                             </div>
 
-                            <div className="flex items-center gap-1">
+                            <div className="flex items-center gap-1 w-[190px]">
                                 <img src={EmailICon} alt="Email-icon" width={24} height={24} />
                                 <p className='text-[#888888] text-[12px] ml-1'>Fitmakerrr@Gmail.com</p>
                             </div>
