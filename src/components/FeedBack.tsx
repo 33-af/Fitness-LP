9f38c6c1ac189afff8724463b4d6f8731532a088
bohdan.snopok <snopokbogdan3@gmail.com>
2025-03-30 10:40:51 +0300

    add 1080-1200px adaptive in 3 blocks

--- src/components/FeedBack.tsx
+++ src/components/FeedBack.tsx
@@ -7,32 +7,34 @@ import Quote from '../assets/quote.png'
 
 const FeedBack = () => {
     return (
-        <section className="custom-container mb-20 ">
-            <div className='relative z-0 flex  items-end w-full h-[350px]'>
-                <div className=" z-10  bg-[#77060B] w-[513px] h-[229px] blur-[200px] rounded-full absolute top-[121px] left-[382px]" />
-                <img src={LeftMenImage} alt="men " width={273} height={350} />
+        <section className="mb-20 lg:px-[46px]">
+            <div className='custom-container'>
+                <div className='relative z-0 flex justify-between items-end w-full h-[350px]'>
+                    <div className=" z-10  bg-[#77060B]  blur-[200px] rounded-full absolute top-[121px] left-[382px]" />
+                    <img src={LeftMenImage} alt="men "/>
 
-                <div className="bg-[#5B0408] w-[623px] h-[192px] rounded-[12px] relative z-50  py-4 px-4 ">
-                    <div className="flex  items-center justify-between mb-3">
-                        <div className="flex flex-col mb-3">
-                            <h2 className='text-white text-[20px]'>Steven Haward</h2>
-                            <span className='text-sm text-[#8B8B8B] font-medium'>Our Trainer</span>
+                    <div className=" bg-[#5B0408] w-[460px] full h-fit rounded-[12px] relative z-50  py-4 px-4 ">
+                        <div className="flex  items-center justify-between mb-3">
+                            <div className="flex flex-col mb-3">
+                                <h2 className='text-white text-[20px]'>Steven Haward</h2>
+                                <span className='text-sm text-[#8B8B8B] font-medium'>Our Trainer</span>
+                            </div>
+                            <img src={Quote} alt="quote-icon " width={38} height={29} />
                         </div>
-                        <img src={Quote} alt="quote-icon " width={38} height={29} />
+                        <p className='xl:text-xs lg:text-[10px] size text-white'>I’ve Been Using Fitmaker For The Past Three Months, And I’m Genuinely Impressed. The Website Is Easy To Navigate, And Everything Is Laid Out Clearly. I Purchased The Premium Plan, And The Personalized Coaching Has Been A Game-Changer For Me. My Coach Is Incredibly Supportive And Always Available To Answer My Questions. The Weekly Video Sessions Keep Me Motivated, And The Custom Meal Plans Have Helped Me Stay On Track With My Goals. Highly Recommended For Anyone Serious About Their Fitness Journey!</p>
                     </div>
-                    <p className='text-xs text-white'>I’ve Been Using Fitmaker For The Past Three Months, And I’m Genuinely Impressed. The Website Is Easy To Navigate, And Everything Is Laid Out Clearly. I Purchased The Premium Plan, And The Personalized Coaching Has Been A Game-Changer For Me. My Coach Is Incredibly Supportive And Always Available To Answer My Questions. The Weekly Video Sessions Keep Me Motivated, And The Custom Meal Plans Have Helped Me Stay On Track With My Goals. Highly Recommended For Anyone Serious About Their Fitness Journey!</p>
-                </div>
 
-                <div className="flex items-center gap-3 ml-6">
-                    <button className="p-0 bg-transparent border-none"><img src={ArrowLeftIcon} alt="arrow-left icon" width={36} height={36} /></button>
-                    <button className="p-0 bg-transparent border-none"><img src={ArrowRightIcon} alt="arrow-right icon" width={36} height={36} /></button>
-                </div>
+                    <div className="flex items-center gap-3 ml-6">
+                        <button className="p-0 bg-transparent border-none"><img src={ArrowLeftIcon} alt="arrow-left icon" width={36} height={36} /></button>
+                        <button className="p-0 bg-transparent border-none"><img src={ArrowRightIcon} alt="arrow-right icon" width={36} height={36} /></button>
+                    </div>
 
-                <div className="flex gap-4 ml-6">
-                    <div className="absolute z-10 top-0 right-0 blur-[145px] bg-[#712B0D] w-[261px] h-[350px] rounded-r-full " />
-                    <div className="relative z-20 flex gap-4">
-                        <img src={RightMenFirstImage} alt="men image" width={100} height={350} />
-                        <img src={RightSecondMenImage} alt="men image" width={100} height={350} />
+                    <div className="flex gap-4 ml-6 none">
+                        <div className="absolute z-10 top-0 right-0 blur-[145px] bg-[#712B0D] w-[261px] h-[350px] rounded-r-full " />
+                        <div className="relative z-20 flex gap-4">
+                            <img src={RightMenFirstImage} alt="men image" />
+                            <img src={RightSecondMenImage} alt="men image" />
+                        </div>
                     </div>
                 </div>
             </div>
--- src/components/Trainers.tsx
+++ src/components/Trainers.tsx
@@ -1,5 +1,5 @@
-import arrowLeft from '../assets/arrow-left.png';
-import arrowRight from '../assets/arrow-right.png'
+import arrowLeft from "../assets/arrow-left.png"
+import arrowRight from "../assets/arrow-right.png"
 import TrainerOneImage from '../assets/samCole (1).png'
 import TrainerTwoImage from '../assets/MH.png'
 import TrainerThreeImage from '../assets/JA.png'
@@ -10,9 +10,10 @@ import { motion } from 'framer-motion';
 
 const Trainers = () => {
     return (
-        <section className="w-[1244px] my-20 mx-auto ">
-            <div className="flex items-center justify-between">
-                <h2 className='text-white font-bold text-[32px]'>Meet Our <span className='text-[#D90A14]'>Trainers</span></h2>
+        <section className=" my-20 mx-auto lg:px-[46px]">
+            <div className='custom-container'>
+                <div className="flex items-center justify-between">
+                <h2 className='text-white font-bold xl:text-[32px] lg:text-[26px]'>Meet Our <span className='text-[#D90A14]'>Trainers</span></h2>
                 <div className="flex items-start flex-col">
                     <div className="flex items-center gap-3 mb-3">
                         <img src={arrowLeft} alt="arrow-left" width={36} height={36} className=' cursor-pointer ' />
@@ -24,64 +25,57 @@ const Trainers = () => {
                     </div>
                 </div>
             </div>
-            <p className='text-white text-base my-6 text-center relative '>At This Part you can See Few Of The Many Positive reviews Of Our Customers.</p>
-            <div className="relative w-full h-[394px] flex   justify-between  z-0">
-                <div className="mt-[258px]  relative z-0 blur-[100px] bg-[#712B0D] w-full h-[94px] rounded-r-full " />
-                <div className="absolute z-50 flex items-center justify-between w-full">
-                    <motion.img
-                        src={TrainerOneImage}
-                        alt="trainer image"
-                        width={277}
-                        height={385}
-                        loading="lazy"
-                        whileHover={{ scale: 1.1 }}
-                        style={{ cursor: "pointer" }}
-                    />
-                    <motion.img
-                        src={TrainerTwoImage}
-                        alt="trainer image"
-                        width={277}
-                        height={385}
-                        loading="lazy"
-                        whileHover={{ scale: 1.1 }}
-                        style={{ cursor: "pointer" }}
-                    />
-                    <motion.img
-                        src={TrainerThreeImage}
-                        alt="trainer image"
-                        width={277}
-                        height={385}
-                        loading="lazy"
-                        whileHover={{ scale: 1.1 }}
-                        style={{ cursor: "pointer" }}
-                    />
-                    <motion.img
-                        src={TrainerFourImage}
-                        alt="trainer image"
-                        width={277}
-                        height={385}
-                        loading="lazy"
-                        whileHover={{ scale: 1.1 }}
-                        style={{ cursor: "pointer" }}
-                    />
+                <p className='text-white xl:text-base lg:text-[14px] my-6 text-center relative '>At This Part you can See Few Of The Many Positive reviews Of Our Customers.</p>
+                <div className="relative w-full h-[394px] flex   justify-between  z-0">
+                    <div className="mt-[258px  relative z-0 blur-[100px] bg-[#712B0D] w-full h-[94px] rounded-r-full " />
+                    <div className="absolute z-50 grid grid-cols-4 gap-10 w-full">
+                        <motion.img
+                            src={TrainerOneImage}
+                            alt="trainer image"
+                            loading="lazy"
+                            whileHover={{ scale: 1.1 }}
+                            style={{ cursor: "pointer" }}
+                        />
+                        <motion.img
+                            src={TrainerTwoImage}
+                            alt="trainer image"
+                            loading="lazy"
+                            whileHover={{ scale: 1.1 }}
+                            style={{ cursor: "pointer" }}
+                        />
+                        <motion.img
+                            src={TrainerThreeImage}
+                            alt="trainer image"
+                            loading="lazy"
+                            whileHover={{ scale: 1.1 }}
+                            style={{ cursor: "pointer" }}
+                        />
+                        <motion.img
+                            src={TrainerFourImage}
+                            alt="trainer image"
+                            loading="lazy"
+                            whileHover={{ scale: 1.1 }}
+                            style={{ cursor: "pointer" }}
+                        />
+                    </div>
+                </div>
+                <div className="flex items-center justify-center">
+                    <motion.button
+                        type='button'
+                        whileHover="hover"
+                        className='w-[102px]  bg-transparent py-2 px-4 border-[1px]
+                    border-solid border-[#CD4E17] flex items-end text-[#CD4E17] text-sm rounded-[12px] mt-6 cursor-pointer  relative overflow-hidden'>
+                        <motion.span
+                            className="absolute inset-0 bg-gradient-to-r from-transparent via-[#CD4E17] to-transparent opacity-50"
+                            initial={{ x: "-100%" }}
+                            animate={{ x: "100%" }}
+                            transition={{ repeat: Infinity, duration: 5, ease: "linear" }}
+                        />
+                        <span className="relative z-10"> View  All</span>
+                        <img src={arrowRightButton} alt="arrow-right" className='ml-2' />
+                    </motion.button>
                 </div>
             </div>
-            <div className="flex items-center justify-center">
-                <motion.button
-                    type='button'
-                    whileHover="hover"
-                    className='w-[102px]  bg-transparent py-2 px-4 border-[1px]
-                    border-solid border-[#CD4E17] flex items-end text-[#CD4E17] text-sm rounded-[12px] mt-6 cursor-pointer  relative overflow-hidden'>
-                    <motion.span
-                        className="absolute inset-0 bg-gradient-to-r from-transparent via-[#CD4E17] to-transparent opacity-50"
-                        initial={{ x: "-100%" }}
-                        animate={{ x: "100%" }}
-                        transition={{ repeat: Infinity, duration: 5, ease: "linear" }}
-                    />
-                    <span className="relative z-10"> View  All</span>
-                    <img src={arrowRightButton} alt="arrow-right" className='ml-2' />
-                </motion.button>
-        </div>
         </section >
     )
 }
